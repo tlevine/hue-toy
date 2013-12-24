289a27fc6ccad8c5041d68266132774339b37b51
Thomas Levine <occurrence@thomaslevine.com>
2013-12-24 22:20:03 +0000

    hex

--- index.js
+++ index.js
@@ -9,14 +9,21 @@ client.lights(app)
 
 function app(err, result) {
   var l = getNames(result)
-  client.on(l.Center)
-  client.on(l.Left)
+  var m = [l.Left,l.Center,l.Right]
+  console.log(toHex(82, 82, 23))
+}
 
-  function getNames(result) {
-    var out = {}
-    for (k in result) {
-      out[result[k].name] = 1 * k
-    }
-    return out
+function getNames(result) {
+  var out = {}
+  for (k in result) {
+    out[result[k].name] = 1 * k
+  }
+  return out
+}
+
+function toHex(r,g,b) {
+  return [r,g,b].map(oneChannel)
+  function oneChannel(decimal) {
+    return (decimal % 255).toString(16)
   }
 }
